874faf15bb1f77ae9f905a143c16fef6d2a7fddc
Albert843 <nuralbert@yandex.ru>
2023-11-30 19:51:54 +0300

    added files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /**
+         * При создании экземпляра типа <Worker> зарплата - фиксированная, вводится при создании экземпляра.
+         */
+        Worker worker1 = Worker.create("Ivan", "Ivanov", 38, "builder", 100_000.0);
+        Worker worker2 = Worker.create("Peter", "Petrov", 48, "fireman", 150_000.0);
+        Worker worker3 = Worker.create("Sergey", "Sergeev", 26, "mechanic", 90_000.0);
+        /**
+         * При создании экземпляра типа <FreeLancer> зарплата - тарифная ставка, вводится при создании экземпляра
+         *      и вычисляется по формуле (20.8 * 8 * тарифная ставка).
+         */
+        FreeLancer freeLancer1 = FreeLancer.create("Anna", "Ivanova", 35, "seller", 500.0);
+        FreeLancer freeLancer2 = FreeLancer.create("Vasily", "Vasiliev", 25, "driver", 450.0);
+
+        /**
+         * Список работников и вывод на экран
+         */
+        List<Employee> employees = new ArrayList<Employee>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(freeLancer1);
+        employees.add(freeLancer2);
+
+        for (Employee employee:employees) {
+            employee.printDisplayInfo();
+        }
     }
 }
